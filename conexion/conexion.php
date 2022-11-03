0bf70b16b8983523af55470b42214c5cecbe1093
Jose Pumarejo <jose.pumarejo02@gmail.com>
2022-11-03 09:47:55 -0500

    codigo para conectarse a sqlserver

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,2 +1,15 @@
 <?php
 $link = mysqli_connect("localhost", "root", "", "tesoreria");
+
+// $Servername = "DESKTOP-Q42TGKU\JOSE PUMAREJO";
+
+// $connectioninfo = array("Database" => "Tesoreria", "UID" => "DESKTOP-Q42TGKU\JOSE PUMAREJO");
+
+// $con = sqlsrv_connect($Servername, $connectioninfo);
+
+// if ($con) {
+//     echo "conexion exitosa";
+// } else {
+//     echo "fallo conexion";
+//     die(print_r(sqlsrv_errors(), true));
+// }
